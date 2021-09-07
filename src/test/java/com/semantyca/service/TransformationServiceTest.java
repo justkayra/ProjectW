15499a8a8cc69bf428ad772077a3af650b4eaefb
Kaira <justaidajam@gmail.com>
2021-09-07 23:40:08 +0300

    #

--- src/test/java/com/semantyca/service/TransformationServiceTest.java
+++ src/test/java/com/semantyca/service/TransformationServiceTest.java
@@ -1,21 +1,11 @@
 package com.semantyca.service;
 
-import com.semantyca.dto.TransformationRequestDTO;
-import com.semantyca.model.Adjective;
 import com.semantyca.repository.exception.DocumentExists;
 import io.quarkus.test.Mock;
-import io.quarkus.test.junit.QuarkusTest;
-import org.jdbi.v3.core.HandleCallback;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-
-@QuarkusTest
 class TransformationServiceTest {
     private static final String example = "Powerful and user-friendly Continuous Integration " +
             "and Deployment server that works out of the box";
@@ -31,15 +21,7 @@ class TransformationServiceTest {
 
     @Test
     void testProcess() throws DocumentExists {
-        Optional<Adjective> adjective = Optional.of(new Adjective.Builder().setValue("powerful").build());
-        Mockito.when(jdbi.withHandle(any(HandleCallback.class))).thenReturn(adjective);
-        TransformationService service = new TransformationService(jdbi, datamuseService, adjectiveService);
-        TransformationRequestDTO requestDTO = new TransformationRequestDTO.Builder()
-                .setSourceText(example)
-                .build();
 
-        String result = service.process(requestDTO);
-        assertNotNull(result);
 
     }
 
