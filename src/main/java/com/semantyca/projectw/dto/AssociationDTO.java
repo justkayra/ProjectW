02effcf9616bbd1075805186015efa69ca59edc2
Kaira <justaidajam@gmail.com>
2021-09-21 21:17:32 +0300

    #

--- src/main/java/com/semantyca/projectw/dto/AssociationDTO.java
+++ src/main/java/com/semantyca/projectw/dto/AssociationDTO.java
@@ -2,10 +2,13 @@ package com.semantyca.projectw.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import java.util.UUID;
+
 public class AssociationDTO {
     private String id;
     private String value;
     private int emphasisRank;
+    private boolean isBaseWord;
 
     @JsonIgnore
     public String getId() {
@@ -32,4 +35,57 @@ public class AssociationDTO {
     public void setEmphasisRank(int emphasisRank) {
         this.emphasisRank = emphasisRank;
     }
+
+    public boolean isBaseWord() {
+        return isBaseWord;
+    }
+
+    public void setBaseWord(boolean baseWord) {
+        isBaseWord = baseWord;
+    }
+
+    public static class Builder {
+        private String id;
+        private String value;
+        private int emphasisRank;
+        private boolean isBaseWord;
+
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setId(UUID id) {
+            this.id = id.toString();
+            return this;
+        }
+
+        public Builder setValue(String value) {
+            this.value = value;
+            return this;
+        }
+
+        public Builder setEmphasisRank(int emphasisRank) {
+            this.emphasisRank = emphasisRank;
+            return this;
+        }
+
+        public Builder isBaseWord(boolean baseWord) {
+            isBaseWord = baseWord;
+            return this;
+        }
+
+        public AssociationDTO build() {
+            AssociationDTO associationDTO = new AssociationDTO();
+            associationDTO.setId(id);
+            associationDTO.setValue(value);
+            associationDTO.setEmphasisRank(emphasisRank);
+            associationDTO.setBaseWord(isBaseWord);
+            return associationDTO;
+        }
+
+
+    }
+
 }
--- src/main/java/com/semantyca/projectw/facade/WordServiceFacade.java
+++ src/main/java/com/semantyca/projectw/facade/WordServiceFacade.java
@@ -7,8 +7,7 @@ import com.semantyca.projectw.service.WordService;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -39,15 +38,32 @@ public class WordServiceFacade {
         dto.setId(String.valueOf(word.getId()));
         dto.setValue(word.getValue());
         dto.setType(String.valueOf(word.getType()));
-        Map<String, AssociationDTO> ass = wordService.findAssociatedWords(word.getId()).stream().map(tuple -> {
+
+        Map<String, AssociationDTO> ass = wordService.findAssociatedWords(word.getId()).stream()
+                .map(tuple -> {
             AssociationDTO associationDTO = new AssociationDTO();
             associationDTO.setId(String.valueOf(tuple.getUUID(0)));
             associationDTO.setValue(tuple.getString(1));
             associationDTO.setEmphasisRank(tuple.getInteger(2));
             return associationDTO;
         }).collect(Collectors.toMap(AssociationDTO::getId, Function.identity()));
-        dto.setAssociations(ass);
+        ass.put(String.valueOf(word.getId()), new AssociationDTO.Builder()
+                .setValue(word.getValue())
+                .setId(word.getId())
+                .isBaseWord(true).build());
+        dto.setAssociations(sortMap(ass));
         return dto;
     }
+
+    public static LinkedHashMap sortMap(Map<String, AssociationDTO> map) {
+        List<Map.Entry<String, AssociationDTO>> list = new LinkedList(map.entrySet());
+        Collections.sort(list, Collections.reverseOrder(Comparator.comparingInt(dto -> dto.getValue().getEmphasisRank())));
+        LinkedHashMap<String, AssociationDTO> result = new LinkedHashMap();
+        for (Map.Entry<String, AssociationDTO> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+
 
 }
